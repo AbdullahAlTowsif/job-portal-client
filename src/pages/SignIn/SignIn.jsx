69415fe00e4078f960a038652f5bc315ef8c6480
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-12-19 23:14:30 +0600

    cookies axios jwt added

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import AuthContext from "../../context/AuthContext/AuthContext";
 import SocialLogin from "../shared/SocialLogin";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignIn = () => {
     const {signInUser} = useContext(AuthContext);
@@ -21,8 +22,13 @@ const SignIn = () => {
         
         signInUser(email, password)
         .then(result => {
-            console.log(result.user);
-            navigate(from);
+            console.log(result.user.email);
+            // navigate(from);
+            const user = {email: email}
+            axios.post('http://localhost:3000/jwt', user, {withCredentials: true})
+            .then(res => {
+                console.log(res.data);
+            })
         })
         .catch(error => {
             console.log(error);
